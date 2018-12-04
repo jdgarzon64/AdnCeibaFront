c067ebf733d9bebb75bd6150cd1ee9962b9d0a4e
jdgarzon64 <jdgarzon64@misena.edu.co>
2018-12-04 16:40:22 -0500

    BUTTONS AND FORMS WORKING

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -11,19 +11,31 @@ import { Vehicle } from '../model/vehicle';
 export class HomeComponent implements OnInit {
   matcher = new ErrorState();
   vehicle: Vehicle = new Vehicle();
-  vehicleType = ['Car','Motorcycle'];
+  vehicleType = ['Car', 'Motorcycle'];
   registryVehicle: FormGroup;
-  constructor(private fb: FormBuilder) { 
+  paymentVehicle: FormGroup;
+  constructor(private fb: FormBuilder) {
     this.buildForm();
   }
 
   ngOnInit() {
- 
+
   }
   buildForm() {
     this.registryVehicle = this.fb.group({
       licence: ['', Validators.compose([Validators.required])],
-      engine: ['', Validators.compose([Validators.required])]
+      engine: ['', Validators.compose([Validators.required])],
     });
+
+    this.paymentVehicle = this.fb.group({
+      payLicence: ['', Validators.compose([Validators.required])]
+    });
+  }
+
+  generatePayment() {
+    console.log('dadasdasda');
+  }
+  onSubmit() {
+    console.log('eqwewqewqe');
   }
 }
